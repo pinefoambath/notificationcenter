20251e5dc2fec6b4389453d31f2410fcf6a47630
Andrea Kreideweiss <akreideweiss@gmail.com>
2022-05-23 22:24:49 +0200

    refactor code

--- app/jobs/send_to_push_service_job.rb
+++ app/jobs/send_to_push_service_job.rb
@@ -4,12 +4,10 @@ class SendToPushServiceJob < ApplicationJob
   def perform(new_notification)
     begin
       puts "Starting to send notificaton to push service"
-     
       MockPushService.send(title: new_notification.title, description: new_notification.description, token: "some_unique_device_token")
-
-      puts "Notification sent successfully to push Service 🟢"
+      puts "Notification sent successfully to Push Service 🟢"
     rescue MockPushService::Error => e
-      puts "Send to push service failed 🔴, error: #{e.message}"
+      puts "Send to Push Service failed 🔴, error: #{e.message}"
     end
   end
 end
